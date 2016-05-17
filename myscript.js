dd01004fcdda9cbf290d525cda2896a6a854f282
Anthony Cruz <ACruz@Anthonys-MacBook-Air.local>
2016-05-17 12:29:16 -0400

    NEW CHANGES

---
+++ myscript.js
@@ -0,0 +1,78 @@
+$(function() {
+	"use strict";
+
+	var topoffset = 50; //variable for menu height
+	var slideqty = $('#feature .item').length;
+	var wheight = $(window).height();
+	var randSlide = Math.floor(Math.random()*slideqty);
+
+	$('#featured .item').eq(randSlide).addClass('active');
+	$('.fullheight').css('height', wheight);
+
+
+	//replace IMG inside carousel
+
+	$('#featured .item img').each(function() {
+		var imgSrc = $(this).attr('src');
+		$(this).parent().css({'background-image': 'url('+imgSrc+')'})
+		$(this).remove();
+	})
+
+
+	//adjust height of .fullheight elements on window resize
+
+	$(window).resize(function(){
+		wheight = $(window).height();
+		$('.fullheight').css('height', wheight);
+	})
+
+
+
+	//Activate Scrollspy
+	$('body').scrollspy({
+		target: 'header .navbar',
+		offset: topoffset
+	})
+	var hash = $(this).find('li.active a').attr('href')
+
+	if(hash !== '#featured'){
+	$('header nav').addClass('inbody');
+	} else{
+		$('header nav').removeClass('inbody');
+	}
+
+
+	//add an inbody class to nav
+	$('.navbar-fixed-top').on('activate.bs.scrollspy'), function(){
+		var hash = $(this).find('li.active a').attr('href')
+
+		if(hash !== '#featured'){
+		$('header nav').addClass('inbody');
+		} else{
+			$('header nav').removeClass('inbody');
+		}
+	}
+	
+	$('.navbar a[href*=#]:not([href=#])').click(function() {
+    	if (location.pathname.replace(/^\//,'') === 
+	     	this.pathname.replace(/^\//,'') && 
+	      	location.hostname === this.hostname) {
+	      	var target = $(this.hash);
+	      	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      	if (target.length) {
+	        	$('html,body').animate({
+	          	scrollTop: target.offset().top-topoffset+40
+	        	}, 500);
+	        	return false;
+	      	} //target.length
+    	} //click function
+  	}); //smooth scrolling
+
+
+
+
+	$('#featured').carousel({
+	  interval: 4000
+	});
+
+});
